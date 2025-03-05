78900540eb25d503fa24e90f186532d73da2a54b
MattStellisoft <matthew.anderson@stellisoft.com>
2025-03-05 16:58:40 +0000

    Exporting framework methods

--- src/framework.js
+++ src/framework.js
@@ -1,3 +1,12 @@
 import { applyBrowserFixes } from "./browser-fixes.js";
+import { reactive } from "./reactive.js";
+import { validate } from "./validate.js";
+import { di } from "./di.js";
 
 applyBrowserFixes();
+
+export {
+    reactive,
+    validate,
+    di
+};
