21453a87be62e82b476c057fe8b7e8a5dc42f906
MattStellisoft <matthew.anderson@stellisoft.com>
2025-03-19 11:02:30 +0000

    Bundling files

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
 export default {
-    input: "src/application.js",
-    output: {
-        file: "dist/application.js",
-        format: "esm"
-    }
+    input: {
+        app: 'src/application.js', // main entry point
+        provider: 'src/providers/AppServiceProvider.js', // entry point for the AppServiceProvider
+        // Add more entry points for other providers as needed
+    },
+    output: [
+        {
+          dir: 'dist',
+          format: 'esm', // You can change the format based on your needs (e.g., cjs, esm)
+          entryFileNames: '[name].js', // Use the name of the entry point for the output file
+          chunkFileNames: '[name]-[hash].js', // Handle chunked files if necessary
+        },
+    ],
 };
